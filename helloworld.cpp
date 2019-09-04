fb3eac0c183b68d3bf507b7ad0b5153a1815e25e
root <fontana@chapman.edu>
2019-09-04 17:43:33 +0000

    second? commit for helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+//Joely Fontana
+//Hello World Assignment
+
+//include packages
+#include <iostream>
+//use the standard namespace
+using namespace std;
+
+//main method with arguments
+int main(int argc, char**argv)
+{
+  //print "Hello World" to the terminal
+  cout<<"Hello World!"<<endl;
+  //always remember to return 0 to end the method
+  return 0;
+}
